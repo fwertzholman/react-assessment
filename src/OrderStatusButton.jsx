0ee46a2adf900f158ed19d797551c19762cceca0
franciswertz <97327339+franciswertz@users.noreply.github.com>
2025-09-29 23:29:55 +0000

    Feature 3 all tests pass

--- src/OrderStatusButton.jsx
+++ src/OrderStatusButton.jsx
@@ -2,12 +2,12 @@ import React, {useContext} from 'react';
 import {CartContext} from './cart/cartContext';
 
 // OrderStatusButton component
-const OrderStatusButton = () => {
+const OrderStatusButton = ( {onClick} ) => {
   const cart = useContext(CartContext);
 
   return (
     <div className="position-absolute top-0 end-0 m-3">
-      <button className="btn btn-outline-secondary">
+      <button onClick={onClick} className="btn btn-outline-secondary">
         Products in Order: {cart.length}
       </button>
     </div>
